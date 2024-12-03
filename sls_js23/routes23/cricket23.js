4abcb8f2ef75e35565328e01c344fe1f02a2eeae
gopal612 <gopal612@gmail.com>
2024-12-03 16:39:23 +0530

    refactor... sls typescript & javascript

--- sls_js23/routes23/cricket23.js
+++ sls_js23/routes23/cricket23.js
@@ -2,7 +2,7 @@ var cricket23 = {};
 
 cricket23.getPlayers = async (stuff) => { 
     console.log('getplayers function23 ===> ', stuff); 
-    return  { name: 'Dhoni', team: "CSK" } 
+    return  { name: 'Dhoni', team: "CSK", time23: new Date().toISOString(), pathParam23: stuff  } 
 } 
 
 module.exports = cricket23;
